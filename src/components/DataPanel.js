f8376317f73d0fd66383bdfdd7020ebb9877e129
Michael Koderisch <code@koderisch.co.uk>
2020-06-08 21:25:12 +0100

    added a tooltip

--- src/components/DataPanel.js
+++ src/components/DataPanel.js
@@ -1,11 +1,18 @@
 import React from 'react';
 
 const DataPanel = (props) => {
-  const localStatus = props.status.link_status === 'no link' ? 'no' : 'ok';
+  const status = props.status;
+  const displayStatus = status.link_status === 'no link' ? 'no' : 'ok';
   return (
     <div className="panel data" style={props.style}>
-      <div className={`icon ${localStatus}`}></div>
+      <div className={`icon ${displayStatus}`}></div>
       <div className="text">{props.name}</div>
+      <div className="panelTooltip">
+        <ul>
+          <li>Link status: {status.link_status}</li>
+          <li>Data rate: {status.bps}bps</li>
+        </ul>
+      </div>
     </div>
   );
 };
